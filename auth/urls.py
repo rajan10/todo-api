2ba8635e55a05c5a5739f49124a201ee60ebc216
Rajan Gauchan <hirajan10@hotmail.com>
2023-12-18 00:04:46 -0500

    Add excection handling to the authentication

--- auth/urls.py
+++ auth/urls.py
@@ -1,5 +1,6 @@
 from flask import Blueprint, request, jsonify, make_response
 from user.user_repo import UserRepo
+from pydantic import ValidationError
 
 from auth.utils import compare_password
 import base64
@@ -8,19 +9,33 @@ import base64
 auth_blueprint = Blueprint("auth_blueprint", __name__)
 
 
-@auth_blueprint.route("/auth", methods=["POST"]) 
+@auth_blueprint.route("/auth", methods=["POST"])
 def user_auth():
-    auth_header = request.headers.get("Authorization")
-    encoded_credential = auth_header.split(" ")[1]
-    decoded_credential = base64.b64decode(encoded_credential).decode("utf-8")
-    username, password = decoded_credential.split(":")
-    user_repo = UserRepo()
-    user = user_repo.read_by_username(username=username)
-    if user:
+    try:
+        auth_header = request.headers.get("Authorization")
+    except Exception as exc:
+        return make_response(jsonify({"message": exc.errors()}))
+        encoded_credential = auth_header.split(" ")[1]
+        decoded_credential = base64.b64decode(encoded_credential).decode("utf-8")
+        username, password = decoded_credential.split(":")
+        user_repo = UserRepo()
+        user = user_repo.read_by_username(username=username)
+        if not user:
+            raise ValueError("No User!")
         result = compare_password(user_password=password, db_password=user.password)
-    if result == True:
-        response = make_response(jsonify({"message": "Successfully authenticated!"}))
-        return response
+        if result:
+            response = make_response(
+                jsonify({"message": "Successfully authenticated!"})
+            )
+            return response
+        else:
+            response = make_response(
+                jsonify({"message": "Not Successfully authenticated!"})
+            )
+            return response
+    except ValidationError as exc:
+        return make_response(jsonify({"message": exc.errors()}), 400)
+    except Exception as exc:
+        return make_response(jsonify({"message": str(exc)}), 500)
 
-    response = make_response(jsonify({"message": "Not Successfully authenticated!"}))
-    return response
+    return jsonify({"error": "Unexpected error occurred"}), 500
