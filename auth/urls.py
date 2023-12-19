561c42ea748a2f8b01e3642f07bc255b545c59bf
Rajan Gauchan <hirajan10@hotmail.com>
2023-12-18 21:50:38 -0500

    Add authentication using decorator

--- auth/urls.py
+++ auth/urls.py
@@ -2,7 +2,7 @@ from flask import Blueprint, request, jsonify, make_response
 from user.user_repo import UserRepo
 from pydantic import ValidationError
 
-from auth.utils import compare_password
+from auth.utils import compare_password, login_required
 import base64
 
 
@@ -13,15 +13,17 @@ auth_blueprint = Blueprint("auth_blueprint", __name__)
 def user_auth():
     try:
         auth_header = request.headers.get("Authorization")
-    except Exception as exc:
-        return make_response(jsonify({"message": exc.errors()}))
         encoded_credential = auth_header.split(" ")[1]
         decoded_credential = base64.b64decode(encoded_credential).decode("utf-8")
         username, password = decoded_credential.split(":")
+        if not username or not password:
+            return make_response(
+                jsonify({"message": "No Username or Password provided"}), 400
+            )
         user_repo = UserRepo()
         user = user_repo.read_by_username(username=username)
         if not user:
-            raise ValueError("No User!")
+            return make_response(jsonify({"message": "No user"}), 401)
         result = compare_password(user_password=password, db_password=user.password)
         if result:
             response = make_response(
@@ -38,4 +40,9 @@ def user_auth():
     except Exception as exc:
         return make_response(jsonify({"message": str(exc)}), 500)
 
-    return jsonify({"error": "Unexpected error occurred"}), 500
+
+@auth_blueprint.route("/hello-world", methods=["GET"])
+@login_required
+def hello_world():
+    print("Executing business logic")
+    return make_response(jsonify({"message": "Hello world!"}), 200)
--- auth/utils.py
+++ auth/utils.py
@@ -1,4 +1,9 @@
 import bcrypt
+from flask import jsonify, request, make_response
+import base64
+from user.user_repo import UserRepo
+
+from pydantic import ValidationError
 
 
 def encrypt_string(string: str):
@@ -27,6 +32,33 @@ def compare_password(user_password: str, db_password: str) -> bool:
 
 # print(compare_password("abc123", db_password_bytes))
 
-# jwt
-# oAuth
 
+def login_required(func):
+    def inner_func(*args, **kwargs):
+        try:
+            auth_header = request.headers.get("Authorization")
+            encoded_credential = auth_header.split(" ")[1]
+            decoded_credential = base64.b64decode(encoded_credential).decode("utf-8")
+            username, password = decoded_credential.split(":")
+            if not username or not password:
+                return make_response(
+                    jsonify({"message": "No Username or Password provided"}), 400
+                )
+            user_repo = UserRepo()
+            user = user_repo.read_by_username(username=username)
+            if not user:
+                return make_response(jsonify({"message": "No user"}), 401)
+            result = compare_password(user_password=password, db_password=user.password)
+            if result:
+                return func(*args, **kwargs)
+            else:
+                response = make_response(
+                    jsonify({"message": "Not Successfully authenticated!"})
+                )
+                return response
+        except ValidationError as exc:
+            return make_response(jsonify({"message": exc.errors()}), 400)
+        except Exception as exc:
+            return make_response(jsonify({"message": str(exc)}), 500)
+
+    return inner_func
